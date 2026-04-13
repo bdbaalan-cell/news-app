715490731375bfb7bca4432d15a8324f00e41aaa
baalan <bdbaalan@gmail.com>
2026-04-13 21:29:42 +0530

    fixed cors properly

--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ const multer = require("multer");
 const jwt = require("jsonwebtoken");
 
 const app = express();
+app.use(cors({
+  origin: "*"
+}));
+
+app.use(express.json());
 
 app.use(cors());
 app.use(express.json());
